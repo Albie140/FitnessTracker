c2d0f67f1df55e523a92f063ff04c67e29d38a6c
Alberte Laventure <alberte.laventure@gmail.com>
2020-10-22 17:02:36 -0400

    deploying to Heroku

--- server.js
+++ server.js
@@ -18,9 +18,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", { useNewUrlParser: true, useUnifiedTopology: true,
-useCreateIndex: true,
-useFindAndModify: false });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 //API Routes
 app.get("/api/workouts", (req, res) => {
