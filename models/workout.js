212efc2813cc44d1d7649be2a4540bae880db411
Alberte Laventure <alberte.laventure@gmail.com>
2020-10-21 23:36:48 -0400

    Added some api routes

--- models/workout.js
+++ models/workout.js
@@ -2,11 +2,48 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: Date,
-    exercises: Array
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Enter an exercise duration in minutes"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+},
+{
+        toJSON: {
+            virtuals: true
+        }
 
-    // need more info
 });
+
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require ("path");
-const logger = require ("morgan");
+const path = require("path");
+const logger = require("morgan");
+const Workout = require("./models/workout");
 
 const app = express();
 
@@ -10,14 +11,62 @@ app.use(logger("dev"));
 const PORT = process.env.PORT || 5082
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true });
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
-//API Routes
-// POST /api/workouts
-// PUT /api/workouts/:id
-// GET /api/workouts/range (Last 7 days: Limit (7))
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", { useNewUrlParser: true });
 
+//API Routes
+app.get("/api/workouts", (req, res) => {
+    Workout.find({
+
+    }).then(
+        (workouts) => {
+                console.log(workouts);
+            res.json(workouts)
+        
+        }).catch((err) => {
+            res.json(err)
+        });
+});
+
+// POST /api/workouts
+app.post("/api/workouts", (req, res) => {
+    Workout.create({
+
+    }).then(
+        (err, workouts) => {
+            if (err)
+                console.log(err);
+            res.json(workouts)
+        });
+});
+// PUT /api/workouts/:id
+app.put("/api/workouts/:id", (req, res) => {
+// console.log(req.params.id)
+    Workout.findByIdAndUpdate(req.params.id, {
+        $push:{exercises: req.body}
+
+    }).then(
+        (err, workouts) => {
+            if (err)
+                console.log(err);
+            res.json(workouts)
+        });
+});
+
+
+// GET /api/workouts/range (Last 7 days: Limit (7))
+// app.get("/api/workouts/range", (req, res) => {
+// db.Workout.find({}, (err, data) => {
+//     if(err)
+//     console.log(err);
+//     res.json(data)
+// });
+// });
 
 //HTML Routes
 app.get("/", (req, res) => {
@@ -32,5 +81,5 @@ app.get("/stats", (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log( `App is running on http://localhost:${PORT}`);
+    console.log(`App is running on http://localhost:${PORT}`);
 });
